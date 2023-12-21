25a0fc9a1fabbcfe7443fb2830dc137adeb0e2c7
Alireza Mohammadi <mohammadialireza238@gmail.com>
2023-12-22 00:50:09 +0100

    Adds route and config file.

--- src/LaravelFileExplorerServiceProvider.php
+++ src/LaravelFileExplorerServiceProvider.php
@@ -6,7 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelFileExplorerServiceProvider extends ServiceProvider
 {
-    public function boot() {}
+    public function boot(): void {
+        //publish config
+        $this->publishes([
+            __DIR__.'/config/laravel-file-explorer.php' => config_path('laravel-file-explorer.php'),
+        ]);
+
+        //publish routes
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+    }
 
     public function register() {}
 }
